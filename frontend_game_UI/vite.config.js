39a8c6a94f0bc215623bbcadb55d8e6ed6981711
svdpmukherjee <100602182+svdpmukherjee@users.noreply.github.com>
2025-01-06 17:39:34 +0100

    vite.config updated

--- frontend_game_UI/vite.config.js
+++ frontend_game_UI/vite.config.js
@@ -1,17 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8000";
-
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "../static",
-  },
   server: {
     proxy: {
       "/api": {
-        target: apiUrl,
+        target: "https://anagram-game-cheating-detection.onrender.com",
         changeOrigin: true,
       },
     },
